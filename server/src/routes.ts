67ae9e673c1dff7cfbb6a9ed0fd1f358464720be
Gabriel Babler <gabrielbabler@yahoo.com.br>
2020-06-02 14:03:06 -0300

    day 2 - part 1

---
+++ server/src/routes.ts
@@ -0,0 +1,46 @@
+import express from 'express';
+import knex from './database/connection';
+
+const routes = express.Router();
+
+routes.get('/items', async (req, res) => {
+  const items = await knex('items').select('*');
+
+  const serializedItems = items.map(item => {
+    return {
+      id: item.id,
+      title: item.title,
+      image_url: `http://localhost:3333/uploads/${item.image}`
+    }
+  })
+
+  res.json(serializedItems);
+});
+
+routes.post('/points', async (req, res) => {
+  const {
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+    items
+  } = req.body;
+
+  await knex('points').insert({
+    image: 'image-fake',
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf
+  });
+
+  return res.json({ success: true });
+});
+
+export default routes;
