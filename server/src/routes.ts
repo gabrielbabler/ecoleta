d4896ee219f135ed598b873ec84b3ba0e47d3e28
Gabriel Babler <gabrielbabler@yahoo.com.br>
2020-06-06 20:22:13 -0300

    fixgin

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -10,7 +10,6 @@ const pointsController = new PointsController();
 const itemsController = new ItemsController();
 
 routes.get('/items', itemsController.index);
-
 routes.post('/points', pointsController.create);
 routes.get('/points/:id', pointsController.show);
 routes.get('/points', pointsController.index);
